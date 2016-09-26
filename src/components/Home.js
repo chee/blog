c8e16bc7b59d0727394141d4d52ee3b6d17b3067
chee <chee@snaek.org>
2016-09-26 20:36:53 +0100

    move titleLink logic into <Post/>

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,12 +7,7 @@ import Footer from './Footer'
 function makePosts(postdata, posts) {
 	return postdata.slice(0, 10).map(({ slug }, index) => {
 		const post = {...posts[slug]}
-		post.title = (
-			<h1 className="post-title">
-				<Link className="post-title-link" to={`/post/${slug}`}>{post.meta.title}</Link>
-			</h1>
-		)
-		post.__html = post.__html.replace(/.*\n/, '')
+		post.titleLink = true
 		return <Post post={post} key={index}/>
 	})
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -6,7 +6,7 @@ import Time from './Time'
 const Post = ({ post }) => (
 	<article className="post">
 		<Time date={post.meta.date}/>
-		{ post.title &&
+		{ post.titleLink && (post.__html = post.__html.replace(/.*\n/, '')) &&
 			<h1 className="post-title">
 				<Link className="post-title-link" to={`/post/${post.meta.slug}`}>{post.meta.title}</Link>
 			</h1>
