2a35dca705b558814d6bd144c52ad147718c48b1
chee <chee@snaek.org>
2016-09-26 20:33:10 +0100

    create <Time/>

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router'
 
+import Time from './Time'
+
 const Post = ({ post }) => (
 	<article className="post">
-		<time className="post-date" dateTime={(new Date(post.meta.date)).toISOString().replace(/...\..*/, '').replace('T', ' ')}>{(new Date(post.meta.date)).toDateString().toLowerCase()}</time>
+		<Time date={post.meta.date}/>
 		{ post.title &&
 			<h1 className="post-title">
 				<Link className="post-title-link" to={`/post/${post.meta.slug}`}>{post.meta.title}</Link>
