d898591d26434499901d77ac7e6bf5fec291bee8
chee <chee@snaek.org>
2017-01-13 21:07:03 +0000

    make the layout a stateless component

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import {Route, IndexRoute} from 'react-router'
 
-import Application from './containers/Application'
+import Layout from './components/Layout'
 import Home from './components/Home'
 import PostAlone from './components/PostAlone'
 import Posts from './components/Posts'
 
 export default (
-	<Route path="/" component={Application}>
+	<Route path="/" component={Layout}>
 		<IndexRoute component={Home}/>
 		<Route path="posts" component={Posts}/>
 		<Route path="post/:slug" component={PostAlone}/>
