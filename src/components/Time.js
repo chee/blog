0d1a725476e7395d777d573d52c15a6ccb75f255
chee <chee@snake.dog>
2018-01-11 09:34:55 +0000

    Cleanly format the time component
    don't try to be gross and magic dust

--- src/components/Time.js
+++ src/components/Time.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const Time = ({date}) => (
-  <time dateTime={(new Date(date)).toISOString().replace(/...\..*/, '').replace('T', ' ')}>
-    {(new Date(date)).toDateString().toLowerCase()}
-  </time>
-)
+const Time = ({date, ...props}) => {
+  const dateObject = new Date(date)
+
+  const dateTime = dateObject
+    .toISOString()
+    .replace(/...\..*/, '')
+    .replace('T', ' ')
+
+  const handsomeDate = dateObject
+    .toDateString()
+    .toLowerCase()
+
+  return (
+    <time
+      {...props}
+      dateTime={dateTime}>
+      {handsomeDate}
+    </time>
+  )
+}
 
 export default Time
